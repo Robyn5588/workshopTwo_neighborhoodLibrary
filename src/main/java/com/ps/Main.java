a5e3b646dbd75adb24c063f5c65bcb5b0f5b5a0f
Robyn <robyncab01@gmail.com>
2025-04-20 00:08:45 -0400

    Added array method that has each Book object

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -11,6 +11,54 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        Book[] library = library();
+
+
+
+
+//        System.out.println(book1.getTitle());
+
+        for(Book book:library){
+            System.out.println(book.getId());
+            System.out.println(book.getIsbn());
+            System.out.println(book.getTitle());
+
+        }
+
+
+    }
+
+
+    public static Book[] library(){
+
+        Book[] book = new Book[20];
+
+        book[0] = new Book(1,"9780393285000",inventory[0],true,"");
+        book[1]  = new Book(2,"9780451530578",inventory[1],true,"");
+        book[2]  = new Book(3,"9780451530783",inventory[2],true,"");
+        book[3]  = new Book(4,"9780177456637",inventory[3],true,"");
+        book[4]  = new Book(5,"9784594550431",inventory[4],true,"");
+        book[5]  = new Book(6,"9781574057104",inventory[5],true,"");
+        book[6]  = new Book(7,"9783508532454",inventory[6],true,"");
+        book[7]  = new Book(8,"9784957391495",inventory[7],true,"");
+        book[8]  = new Book(9,"9785430543145",inventory[8],true,"");
+        book[9]  = new Book(10,"97849205940285",inventory[9],true,"");
+        book[10]  = new Book(11,"97845739548754",inventory[10],true,"");
+        book[11]  = new Book(12,"97859075984375",inventory[11],true,"");
+        book[12]  = new Book(13,"97834584504883",inventory[12],true,"");
+        book[13]  = new Book(14,"97802574593035",inventory[13],true,"");
+        book[14]  = new Book(15,"97823450243544",inventory[14],true,"");
+        book[15]  = new Book(16,"97824505405752",inventory[15],true,"");
+        book[16]  = new Book(17,"97823432504098",inventory[16],true,"");
+        book[17]  = new Book(18,"97804540520457",inventory[17],true,"");
+        book[18]  = new Book(19,"97862405434500",inventory[18],true,"");
+        book[19]  = new Book(20,"97820572034502",inventory[19],true,"");
+
+        return book;
+    }
+
+    public static void homeScreen(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nLibrary Home Screen:");
         System.out.println("1) Show Available Books");
@@ -28,66 +76,7 @@ public class Main {
                 break;
             default:
         }
-
-        Book book1 = new Book(1,"9780393285000",inventory[0],true,"");
-        Book book2 = new Book(2,"9780451530578",inventory[1],true,"");
-        Book book3 = new Book(3,"9780451530783",inventory[2],true,"");
-        Book book4 = new Book(4,"9780177456637",inventory[3],true,"");
-        Book book5 = new Book(5,"9784594550431",inventory[4],true,"");
-        Book book6 = new Book(6,"9781574057104",inventory[5],true,"");
-        Book book7 = new Book(7,"9783508532454",inventory[6],true,"");
-        Book book8 = new Book(8,"9784957391495",inventory[7],true,"");
-        Book book9 = new Book(9,"9785430543145",inventory[8],true,"");
-        Book book10 = new Book(10,"97849205940285",inventory[9],true,"");
-        Book book11 = new Book(11,"97845739548754",inventory[10],true,"");
-        Book book12 = new Book(12,"97859075984375",inventory[11],true,"");
-        Book book13 = new Book(13,"97834584504883",inventory[12],true,"");
-        Book book14 = new Book(14,"97802574593035",inventory[13],true,"");
-        Book book15 = new Book(15,"97823450243544",inventory[14],true,"");
-        Book book16 = new Book(16,"97824505405752",inventory[15],true,"");
-        Book book17 = new Book(17,"97823432504098",inventory[16],true,"");
-        Book book18 = new Book(18,"97804540520457",inventory[17],true,"");
-        Book book19 = new Book(19,"97862405434500",inventory[18],true,"");
-        Book book20 = new Book(20,"97820572034502",inventory[19],true,"");
-
-
-
-
-
-
-//        System.out.println(book1.getTitle());
-
-//        for(String book:inventory){
-//            System.out.println(book);
-//        }
-
-
     }
-
-//    public static void library (){
-//        Book book1 = new Book(1,"9780393285000",inventory[0],true,"");
-//        Book book2 = new Book(2,"9780451530578",inventory[1],true,"");
-//        Book book3 = new Book(3,"9780451530783",inventory[2],true,"");
-//        Book book4 = new Book(4,"9780177456637",inventory[3],true,"");
-//        Book book5 = new Book(5,"9784594550431",inventory[4],true,"");
-//        Book book6 = new Book(6,"9781574057104",inventory[5],true,"");
-//        Book book7 = new Book(7,"9783508532454",inventory[6],true,"");
-//        Book book8 = new Book(8,"9784957391495",inventory[7],true,"");
-//        Book book9 = new Book(9,"9785430543145",inventory[8],true,"");
-//        Book book10 = new Book(10,"97849205940285",inventory[9],true,"");
-//        Book book11 = new Book(11,"97845739548754",inventory[10],true,"");
-//        Book book12 = new Book(12,"97859075984375",inventory[11],true,"");
-//        Book book13 = new Book(13,"97834584504883",inventory[12],true,"");
-//        Book book14 = new Book(14,"97802574593035",inventory[13],true,"");
-//        Book book15 = new Book(15,"97823450243544",inventory[14],true,"");
-//        Book book16 = new Book(16,"97824505405752",inventory[15],true,"");
-//        Book book17 = new Book(17,"97823432504098",inventory[16],true,"");
-//        Book book18 = new Book(18,"97804540520457",inventory[17],true,"");
-//        Book book19 = new Book(19,"97862405434500",inventory[18],true,"");
-//        Book book20 = new Book(20,"97820572034502",inventory[19],true,"");
-//
-//
-//    }
 
     public static void availableBooks(){
         System.out.println("1) Check out");
@@ -95,13 +84,29 @@ public class Main {
         System.out.print("Please Choose an Option: ");
         int availableBookOption = scanner.nextInt();
 
+        switch(availableBookOption){
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+        }
+
     }
 
     public static void checkOutBook (){
         System.out.println("C- Check in Book");
         System.out.println("X- Home Screen");
         System.out.print("Please Choose an Option: ");
-        String checkOutOption = scanner.nextLine();
+        String checkOutOption = scanner.nextLine().toUpperCase();
+
+        switch(checkOutOption){
+            case "C":
+                break;
+            case "X":
+                break;
+            default:
+        }
     }
 
     public static void checkInBook (Book id){
@@ -110,8 +115,5 @@ public class Main {
         id.checkIn();
 
     }
-
-
-
 
 }
